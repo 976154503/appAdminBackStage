e37f7e4e6971f06807ebf2d88c404079c685d858
gao_haohao@126.com <gao_haohao@126.com>
2019-03-04 22:04:11 +0800

    后台项目首页

---
+++ backStage/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+      <temphead></temphead>
+  </div>
+</template>
+
+<script>
+import head from './components/head.vue'
+export default {
+  name: 'App',
+  components: {
+      temphead: head
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ backStage/src/components/head.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <el-row class="el-header bg-primary">
+            <el-col :span="24">
+                <img src="" title="">
+                <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+                    <el-menu-item index="1">处理中心</el-menu-item>
+                    <el-menu-item index="2">切换主题色</el-menu-item>
+                    <el-menu-item index="3">下载主题</el-menu-item>
+                    <el-menu-item index="4">帮助</el-menu-item>
+                    <el-menu-item index="5">中文</el-menu-item>
+                </el-menu>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+<style scoped>
+    .bg-primary {
+        color: #fff;
+    }
+</style>
+
