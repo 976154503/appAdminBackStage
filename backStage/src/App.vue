e75131935c92cf6b3e2b939724ba639227ae3fe5
gao_haohao@126.com <gao_haohao@126.com>
2019-03-05 21:49:36 +0800

    登陆页面

--- backStage/src/App.vue
+++ backStage/src/App.vue
@@ -1,25 +1,83 @@
 <template>
   <div id="app">
-      <temphead></temphead>
+      <div class="el-logo-head">
+        <p>小应用系统</p>
+      </div>
+      <el-form ref="form" :model="form" label-width="80px" class="login-form">
+        <p class="login-title">XXX小程序应用系统</p>
+        <el-input v-model="form.name" placeholder="请输入手机号" class="form-input"></el-input>
+        <el-input placeholder="请输入密码" class="form-input"></el-input>
+        <el-button type="primary" class="fluid mt15 mb15">登&nbsp;&nbsp;陆</el-button>
+      </el-form>
   </div>
 </template>
 
 <script>
-import head from './components/head.vue'
 export default {
   name: 'App',
-  components: {
-      temphead: head
+  data() {
+    return {
+      form: {
+        name: ''
+      }
+    }
   }
 }
 </script>
 
 <style>
+body {
+  background-color: #f6f6f6;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.el-logo-head {
+  background-color: #000;
+}
+
+.el-logo-head p {
+  width: 1180px;
+  height: 40px;
+  line-height: 40px;
+  margin: 0 auto;
+  color: #fff;
+  font-size: 14px;
+  font-weight: bold;
+  text-align: left;
+}
+
+.login-form {
+  width: 280px;
+  padding: 15px 30px;
+  margin: 150px auto 0;
+  background-color: #fff;
+}
+
+.login-title {
+  font-size: 14px;
+  color: #666;
+  text-align: left;
+}
+
+.form-input {
+  margin: 5px auto;
+}
+
+.fluid {
+  width: 100%;
+}
+
+.mt15 {
+  margin-top: 15px;
+}
+
+.mb15 {
+  margin-bottom: 15px;;
 }
 </style>
